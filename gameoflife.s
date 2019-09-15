04a26ebc23fdbce8bc22154ce03b635227c855d4
Adrian Stoll <adrs@umich.edu>
2019-09-15 14:32:26 -0700

    initialize grid

--- gameoflife.s
+++ gameoflife.s
@@ -2,7 +2,7 @@
 
 .section .data
 usage_str:
-	.ascii "usage: gameoflife <size> <generations>\n\0"
+	.ascii "usage: gameoflife <size> <seed> <generations>\n\0"
 mmap_error:
 	.ascii "error: could not allocate memory\n\0"
 rng_state:
@@ -127,9 +127,62 @@ newgrid:
 
 L_newgrid_alloc_succeeded:
 	mov %rax, %rdi
-	pop %rsi # restore side lenght of grid
+	pop %rsi   # restore side length of grid
+	push %rax  # save grid pointer
+	push %rsi  # save side length
 	call cleargrid
-	# TODO: randomly initialize grid
+
+	pop %rsi # restore side length of grid
+	inc %rsi
+
+	# rdi = current cell in grid
+	mov %rax, %rdi
+	# skip the border
+	add %rsi, %rdi
+	inc %rdi
+
+	# rcx = cur row index, rdx = cur col index
+	mov $1, %rcx
+
+	# for each row
+L_init_row_loop:
+	cmp %rsi, %rcx
+	jge L_init_end
+
+	# skip border
+	inc %rdi
+
+	# for each column
+	mov $1, %rdx
+L_init_col_loop:
+	cmp %rsi, %rdx
+	jge L_init_col_end
+
+	# Randomly initialize cell
+	call rand
+	shr $29, %rax
+
+	# # Compute rax mod 5
+	# xor %edx, %edx
+	# mov $5, %rbx
+	# div %rbx
+
+	cmp $0, %rax
+	jne L_init_no_life
+	movb $0x41, (%rdi)
+L_init_no_life:
+	inc %rdi
+
+	inc %rdx
+	jmp L_init_col_loop
+L_init_col_end:
+	# Skip over border
+	inc %rdi
+
+	inc %rcx
+	jmp L_init_row_loop
+L_init_end:
+	pop %rax # restore grid pointer
 	ret
 
 usage:
@@ -167,8 +220,8 @@ L_atoi_end:
 	ret
 
 _start:
-	# Check argc = 3
-	cmpq $3, (%rsp)
+	# Check argc = 4
+	cmpq $4, (%rsp)
 	jz L_main_parse_args
 	call usage
 
@@ -179,14 +232,16 @@ L_main_parse_args:
 	# TODO: Check that grid size > 0
 	mov %rax, %r12 # Calle save
 
-	# Num generations
+	# Seed
 	mov 0x18(%rsp), %rdi
 	call atoi
-	mov %rax, %r13 # Calle save
-
-	# Seed RNG
-	mov %r12, %rdi
+	mov %rax, %rdi
 	call srand
+
+	# Num generations
+	mov 0x20(%rsp), %rdi
+	call atoi
+	mov %rax, %r13 # Calle save
 
 	# Make new grid
 	mov %r12, %rdi
